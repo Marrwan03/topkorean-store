7f47dfd5c27cf1562e71091febd570ecff313d2a
Marwan Alawlaqi <tlooddi107@gmail.com>
2026-05-18 13:55:43 +0300

    Update products.js

--- js/products.js
+++ js/products.js
@@ -492,13 +492,12 @@ function updateResultsCount() {
 
 function getItemsPerPage() {
     const width = window.innerWidth;
-    
-    if (width >= 800) {
-        return 9;      
-    } else if (width >= 576) {
-        return 6;      
-    } else if (width >= 375) {
-        return 4;      
+    if(width >= 1000)
+    {
+        return 8:
+    }
+    else if (width >= 800) {
+        return 6;          
     } else {
         return 4;    
     }
